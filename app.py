4f593202c44ae04b312a61c11a8be1d1dc1d22f8
Snaiper4040 <110387202+Snaiper4040@users.noreply.github.com>
2025-05-14 10:01:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,13 @@ app = Flask(__name__)
 def Determinant(num11, num12, num21, num22):
     return num11 * num22 - num12 * num21
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def index():
     result = None
-    if request.method == 'POST':
-        try:
-            result = Determinant(float(request.form['num11']), float(request.form['num12']), float(request.form['num21']), float(request.form['num22']))
-        except ValueError:
-            result = "Ошибка: Ожидается число!"
+    try:
+        result = Determinant(float(request.form['num11']), float(request.form['num12']), float(request.form['num21']), float(request.form['num22']))
+    except ValueError:
+        result = "Ошибка: Ожидается число!"
     return render_template('index.html', result=result)
 
 if __name__ == '__main__':
